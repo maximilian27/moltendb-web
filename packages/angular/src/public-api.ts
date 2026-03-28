70f8a24375a442f57fa5e63a16f0f3f1684ec338
maximilian27 <maximilian.both27@outlook.com>
2026-03-29 00:21:33 +0200

    feat: angular wrapper package alpha

---
+++ packages/angular/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of @moltendb-web/angular
+ */
+
+export * from './lib/moltendb.provider';
+export * from './lib/moltendb.service';
+export * from './lib/inject-live-query';
