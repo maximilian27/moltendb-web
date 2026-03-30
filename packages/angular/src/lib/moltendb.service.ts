f0a76910018db4564b229b270928b21227a24eb1
maximilian27 <maximilian.both27@outlook.com>
2026-03-30 08:00:57 +0300

    refactor: BREAKING CHANGE - change MoltenDB naming convention to MoltenDb

--- packages/angular/src/lib/moltendb.service.ts
+++ packages/angular/src/lib/moltendb.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, inject, signal } from '@angular/core';
-import { MoltenDB } from '@moltendb-web/core';
-import { MoltenDBClient } from '@moltendb-web/query';
+import { MoltenDb } from '@moltendb-web/core';
+import { MoltenDbClient } from '@moltendb-web/query';
 import { MOLTEN_CONFIG } from './moltendb.provider';
 
 @Injectable({ providedIn: 'root' })
 export class MoltenDbService {
-  public db: MoltenDB;
-  public client: MoltenDBClient;
+  public db: MoltenDb;
+  public client: MoltenDbClient;
 
   // A Signal components can watch to know when WASM is booted and Leader Election is done
   public isReady = signal<boolean>(false);
@@ -15,14 +15,14 @@ export class MoltenDbService {
     // 🚀 Modern Angular Injection (No constructor decorators needed!)
     const config = inject(MOLTEN_CONFIG);
 
-    this.db = new MoltenDB(config.name, config);
-    this.client = new MoltenDBClient(this.db);
+    this.db = new MoltenDb(config.name, config);
+    this.client = new MoltenDbClient(this.db);
 
     // Boot the engine and update the signal when done
     this.db.init().then(() => {
       this.isReady.set(true);
     }).catch(err => {
-      console.error('[MoltenDB] Failed to initialize', err);
+      console.error('[MoltenDb] Failed to initialize', err);
     });
   }
 }
